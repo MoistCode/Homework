016774ce3ebef77f041390af35bf44baf9e52d51
Tommy Pham <phamtommylong1991@gmail.com>
2018-03-06 22:25:59 -0800

    Added some helper methods to user.rb

--- W4D3/Auth_Homework/app/models/user.rb
+++ W4D3/Auth_Homework/app/models/user.rb
@@ -14,4 +14,26 @@ class User < ApplicationRecord
   validates :username, :session_token, presence: true, uniqueness: true
   validates :password_digest, :presence: { message: "Password can't be blank" }
   validates :password, length: { minimum: 6, allow_nil: true }
+  before_validation :ensure_session_token
+
+  def self.find_by_credentials
+
+  end
+
+  def self.generate_session_token
+
+  end
+
+  def reset_session_token!
+
+  end
+
+  def ensure_session_token
+
+  end
+
+  def password=(new_pw)
+
+  end
+  
 end
