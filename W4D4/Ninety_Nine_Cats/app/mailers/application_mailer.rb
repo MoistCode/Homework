3f52685923167aff0169a894d6ab262a756ddbf6
Tommy Pham <phamtommylong1991@gmail.com>
2018-03-07 20:49:14 -0800

    Completed a welcome email format for the user

--- W4D4/Ninety_Nine_Cats/app/mailers/application_mailer.rb
+++ W4D4/Ninety_Nine_Cats/app/mailers/application_mailer.rb
@@ -1,4 +1,12 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'everybody@applacademy.io'
   layout 'mailer'
+
+  def welcome_email(user)
+    @user = user
+    @url = 'http://localhost:3000/cats'
+    mail(to: user.email, subject: 'Did you bring the spam wasubi?')
+  end
+
+
 end
