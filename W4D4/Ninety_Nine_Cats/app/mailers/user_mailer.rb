99997630ce4280aeecbe4b8339d93fe96c4ac364
Tommy Pham <phamtommylong1991@gmail.com>
2018-03-07 21:00:10 -0800

    Completed W4D4 homework

--- W4D4/Ninety_Nine_Cats/app/mailers/user_mailer.rb
+++ W4D4/Ninety_Nine_Cats/app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
     @url = 'http://localhost:3000/cats'
-    mail(to: user.email, subject: 'Did you bring the spam wasubi?')
+    mail(to: user, subject: 'Did you bring the spam wasubi?')
   end
 
 end
